811d415203b298204a51fd9aa7e4d2633d809ddb
MaxPra <epiccmax@gmx.at>
2021-05-17 18:40:19 +0200

    Version 3.0 with JFileChooser, ...

--- src/TreeExample.java
+++ src/TreeExample.java
@@ -1,5 +1,9 @@
 import javax.swing.*;
 import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.util.List;
 
@@ -7,14 +11,64 @@ import java.util.List;
 public class TreeExample extends JFrame
 {
     private JTree tree;
+    private DefaultTreeModel treeMod;
+    private JTextField jtfPath;
+    private JFileChooser jfcPath;
+    private JButton btnChooseFolder;
+    private String path = System.getProperty("user.home") + File.separator + "Desktop";  //Homedirectory as default
     public TreeExample()
     {
-        String path = "D:\\Google Drive\\HTL\\Schuljahr 2021";
+
+        System.out.println(System.getProperty("user.home"));
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLayout(new BorderLayout());
+        this.setTitle("JTree Example");
+
+        JPanel panelNorth = new JPanel(new FlowLayout());
+        //showing current selected dir
+        jtfPath = new JTextField(path);
+        jtfPath.setPreferredSize(new Dimension(200, 20));
+        panelNorth.add(jtfPath);
+
+        //button to choose dir with JFC
+        btnChooseFolder = new JButton("choose");
+        btnChooseFolder.setPreferredSize(new Dimension(100, 20));
+        btnChooseFolder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                jfcPath = new JFileChooser();
+                jfcPath.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+                int res = jfcPath.showOpenDialog(null);
+
+                if(res != JFileChooser.CANCEL_OPTION && res != JFileChooser.ERROR_OPTION){
+                    //Not canceled or error
+                    path = jfcPath.getSelectedFile().getAbsolutePath(); //Get Folderpath user choosed
+
+                    File file = new File(path);
+
+                    //create the root node
+                    DefaultMutableTreeNode root2 = new DefaultMutableTreeNode(file.getName());
+                    if(file.isDirectory()){
+                        List<ListEntry> listEntries = ListFiles.listFiles(path);
+
+                        for (int i = 0; i < listEntries.size(); i++) {
+                            ListFiles.buildTree(listEntries.get(i),root2);
+                        }
+                    }
+
+                    treeMod.setRoot(root2);
+                    treeMod.reload();
+                }
+            }
+        });
+        panelNorth.add(btnChooseFolder);
+        add(panelNorth, BorderLayout.NORTH);
+
         File file = new File(path);
 
         //create the root node
         DefaultMutableTreeNode root = new DefaultMutableTreeNode(file.getName());
-
         if(file.isDirectory()){
             List<ListEntry> listEntries = ListFiles.listFiles(path);
 
@@ -40,11 +94,11 @@ public class TreeExample extends JFrame
         root.add(fruitNode);*/
 
         //create the tree by passing in the root node
-        tree = new JTree(root);
-        add(new JScrollPane(tree));
+        treeMod = new DefaultTreeModel(root);
+        tree = new JTree(treeMod);
+        add(new JScrollPane(tree), BorderLayout.CENTER);
 
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setTitle("JTree Example");
+
         this.pack();
         this.setVisible(true);
     }
